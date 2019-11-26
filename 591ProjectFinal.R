8d2a2e2fd1ea8c87cc1a596f67bfb0f84b9b34b8
autumn-nam <55520388+autumn-nam@users.noreply.github.com>
2019-11-26 16:22:25 -0500

    Add files via upload
    added the test of constant variance part and more detailed explanation of each step

--- 591ProjectFinal.R
+++ 591ProjectFinal.R
@@ -15,7 +15,7 @@ library(readxl)
 
 ## load the data file into R
 getwd()
-Bauman_MMC_Student_Count_Data <- read_excel("C:/Users/udam0/Downloads/Bauman MMC Student Count Data.xlsx")
+Bauman_MMC_Student_Count_Data <- read_excel("C:/Users/nam1w/Desktop/sta589/Bauman MMC Student Count Data.xlsx")
 
 ## Reshape the data format
 longstudent <- melt(Bauman_MMC_Student_Count_Data, id.vars = "Year")
@@ -28,24 +28,46 @@ workdata <- workdata[-103]
 workdata
 
 ## Transform the data to be time series data. 
-ts.Students <- ts(workdata, start = 1968, frequency = 2)
+ts.Students <- ts(workdata, start = 1968, frequency = 2) # The number of students were measured two times in a year from 1968 to 2018.
+                                                         
+######################################################################################
+# Transform Stage 
+######################################################################################
+## exploratory analysis / eventually checking whether our data is stationary or not and the variance is constant or not.  
+
+autoplot(ts.Students) + ggtitle("Number of students for each semester")
+
+## Test for constant variance
+diff(ts.Students)^2 %>% Box.test(type ="Lj")     # p-value is smaller than 0.05. Therefore, we reject the null hypothesis. In other words, Effect of ARCH is present, and the data has non-constant variance. 
+                                                 # Therefore, we need transformation.
+## Transform the data to have constant variance using Box.cox transformation
+ld <- BoxCox.lambda(ts.Students)
+trs.ts.Students <- BoxCox(ts.Students, lambda = ld) 
+
+ggAcf(trs.ts.Students)              # According to the plot, the data has a trend. 
+summary(ur.kpss((trs.ts.Students))) # Make sure it is stationary or not. we use the Kwiatkowski-Phillips-Schmidt-Shin (KPSS) test (Kwiatkowski, Phillips, Schmidt, & Shin, 1992). 
+                                    # In this test, the null hypothesis is that the data are stationary, and we look for evidence that the null hypothesis is false.
+                                    # The test statistic (1.5573) is much greater than the 1% critical value (0.739), indicating tha tthe null hypothesis is rejected. That is, the data is non-stationary.  Results says it is not stationary. 
+                                    # so far, we learned the data is non-stationary and have a trend. In other words, before we go further to make a model, we need transformation. 
+## Differencing
+# When both seasonal and first differences are applied, it makes no difference which is done first-the result will be the same.
+# However, if the data have a strong seasonal pattern, we recommend that seasonal differencing be done first, because the resulting series will sometimes be stationary and there will be no need for a further first difference. If first differencing is done first, there will still be seasonality present.
+
+nsdiffs(trs.ts.Students)
+ndiffs(trs.ts.Students)                              # we need both first and seasoanl differencing
+
+d.trs.ts.Students = diff(trs.ts.Students)            # performed first differencing 
+d4.trs.ts.Students = diff(trs.ts.Students, lag = 2)  # performed seasonal differencing 
+
+d4.trs.ts.Students %>% ndiffs()                      # we've done the seasonal differencing first and checked if we need further differncing
+                                                     # Results says we don't need to do first differncing 
+## Test for Stationary 
+ur.kpss(d4.trs.ts.Students) %>% summary()            # The test statistic 0.1063 is smaller than the 10%, 5%, 5.2%, and1% critival values, indicating that the null hyotheis is failed to rejected. That is the data is stationary. 
 
 ######################################################################################
 # Analysis Stage 
-######################################################################################
-autoplot(ts.Students) + ggtitle("Number of students for each semester")
-ggAcf(ts.Students)              # According to the plot, it has a trend. 
-ndiffs(ts.Students)             # Make sure it needs first differencing using function. Results says yes. 
-summary(ur.kpss((ts.Students))) # Make sure it is stationary or not. Results says it is not stationary. 
-
-d.ts.students = diff(ts.Students)            # performed first differencing 
-d4.ts.students = diff(ts.Students, lag = 4)  # performed seasonal differencing 
-
-ts.Students %>% diff(lag=4) %>% ndiffs()
-
-ggtsdisplay((ts.Students))
-ggtsdisplay(d.ts.students)
-ggtsdisplay(d4.ts.students)
+######################################################################################                                                     # we are now good to go and make a model
+ggtsdisplay(d4.trs.ts.Students)
 
 m2 <- auto.arima(ts.Students)
 m1 = fit <- Arima(ts.Students, order=c(4,0,1), seasonal = c(1,0,0))
